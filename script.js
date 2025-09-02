66e1975267cb81fee5d7f340bd10773a5f5c1165
Nícolas Mussi <nicobocojoly@gmail.com>
2025-09-01 21:53:05 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function enviarMensagem(event) {
+
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5541999005683'
+
+    const texto = `Olá, me chamo ${nome}, ${mensagem}`
+
+    const msgFormatada = encodeURIComponent(texto)
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+    console.log(url)
+
+    window.open(url, '_blank')
+}
